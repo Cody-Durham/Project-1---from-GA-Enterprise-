b1cbc5d8e5ef68db7c60edd6a7b5dc24434e3cb3
Cody Durham <codydd@gmail.com>
2021-04-15 10:51:40 -0600

    Update close button on MOBILE

--- js/app.js
+++ js/app.js
@@ -25,93 +25,79 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1j85QSABivRQO2ZJQvY48sfWTUv_2
 
 ///////////////////////////////////// GLOBAL VARIABLES //////////////////////////////////////
 const $cardContainer = $('#card-container')
-const $closeBtn = $('.close-button1')
+const $closeButton = $('.close-button')
 
 
 /// SHOW PROJECTS ///
 const $projectsDrop = $('.dropdown-menu li:nth-of-type(1)')
 
 $projectsDrop.on('click', (event) => {
+    $('#main').empty
     $($cardContainer).removeClass('hidden')
-    // $('.close-button1').addClass('hidden')
-    // $closeBtn.removeClass('hidden')
+    $('.close-button').removeClass('hidden')
 })
 
 /// CLEAR PROJECTS ///
-$('.close-button1').on('click', (event) => {
+$('.close-button').on('click', (event) => {
     $cardContainer.addClass('hidden') 
 })
 
+/// HIDE CLEAR BUTTON ///
+$closeButton.on('click', (event) => {
+    $('.button-housing').addClass('hidden')
+})
 
 
 /// SHOW CONTACT FORM ///
 const $contactDrop = $('.dropdown-menu li:nth-of-type(2)')
 
 $contactDrop.on('click', (event) => {
+    // $('#main').empty()
+
     $('#main').html(`
     <section class="form-container">
-    <form>
-    <div class="form-group">
-    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    </div>
-    <div class="form-group">
-    <label for="exampleInputPassword1">Phone Number</label>
-    <input type="text" class="form-control" id="exampleInputPassword1">
-    </div>
-    <button type="submit" class="submit-button btn-danger id="submit-button">Submit</button>
-    </form>
+        <form>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Phone Number</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="submit-button btn-danger id="submit-button">Submit</button>
+        </form>
     </section>
     `)
      
     // $('.button-housing').removeClass('hidden')
-    
-    
 })
+
+
+
+/// SHOW CONTACT FORM ///
+const $aboutDrop = $('.dropdown-menu li:nth-of-type(3)')
+
+$aboutDrop.on('click', (event) => {
+    // $('#main').empty()
+    $('#main').html(`
+    <section class="display-about styling">
+        <h1 class="about-me-copy">Born and raised in Colorado Ive learned to love the outdoors and everything it has to
+            offer. Fly Fishing, mountain biking and hunting are some of my favorite things to do when I have time. I
+            also rock some photography and dog training on the side when I have time. </h1><br>
+        <h1 class="about-me-copy">I graduated with a degree in Industrial Design and have worked receintly worked in the
+            outdoor space designing/ developing softgoods (backpacks, tents) for a local outdoor company and loved every
+            aspect of creating products. </h1>
+        
+    </section>
+    `)
+})
+
 
 ////////////////////////////////////// CLEAR CONTACT FORM ////////////////////////////////////////
 
 
-
-//  1st try ********************************************
-const $clearForm = () => {
-    
-    if ($('#submit-button')) {
-        // $('.form-container').trigger('reset')
-        console.log('squibble!')
-    }
-}
-
-
-
-
-
-
-
-
-
-
-// $clearButton.on('click', '$clearProjects')
-
-// const $clearProjects = () => {
-    //     $('#card-container').attr('class', 'hidden')
-    // }
-    // $('#form').empty()
-    
-    // $clearButton.text('Close').appendTo($newDiv)
-    // $clearButton.attr('class', 'close-button')
-    
-    // $clearButton.attr('class', 'close-button')
-    // const $clearButton = $('<button>')
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ 
     
     
     
